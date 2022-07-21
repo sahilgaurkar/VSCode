f67c76b669b24d6275adf14719682d93979d9445
Sahil Gaurkar <sahilgaurkar@gmail.com>
2022-07-21 07:22:33 +0530

    Test

--- Coursera/Python_Basics/Turtle.py
+++ Coursera/Python_Basics/Turtle.py
@@ -1,11 +1,18 @@
+from multiprocessing.connection import wait
 import turtle
 
 wn = turtle.Screen()
 alex = turtle.Turtle()
-alex.forward(150)
-alex.left(90)
-alex.forward(75)
+alex.color("red")
+alex.shape("turtle")
+alex.up()
+distant = 5
+
+for _ in range(30):
+    alex.stamp()
+    alex.forward(distant)
+    alex.right(24)
+    distant = distant + 2
 
 
-
-input()
+wn.exitonclick()
